3d2ded3cd86898f4074eea7908f8cacc9201ac65
Veselin Romić <omegavesko@gmail.com>
2021-05-30 12:10:59 +0200

    feat: working Discord auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 import * as React from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
+
 export interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = () => {
-  return <div>uwu</div>;
+  const [session, loading] = useSession();
+  console.log(session);
+
+  return (
+    <>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default HomePage;
