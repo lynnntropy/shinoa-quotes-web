a001bd04dc074920a6381375ed0dbaa79498a5d1
Veselin Romić <omegavesko@gmail.com>
2021-06-06 15:46:41 +0200

    feat: retrieve and store user guilds

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,73 @@
+import axios from "axios";
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 import Providers from "next-auth/providers";
+
+export interface Guild {
+  id: string;
+  name: string;
+  icon: string;
+  owner: boolean;
+  permissions: string;
+  features: string[];
+}
+
+type GuildsResponse = Guild[];
 
 export default NextAuth({
   providers: [
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      scope: "identify email guilds",
+      async profile(profile, tokens) {
+        let imageUrl: string;
+
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
+          imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+          imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
+
+        const { data: guilds } = await axios.get<GuildsResponse>(
+          "https://discord.com/api/users/@me/guilds",
+          {
+            headers: {
+              Authorization: `Bearer ${tokens.accessToken}`,
+            },
+          }
+        );
+
+        return {
+          id: profile.id,
+          name: profile.username,
+          image: imageUrl,
+          email: profile.email,
+          guilds,
+        };
+      },
     }),
   ],
   callbacks: {
     async session(session, token) {
-      // expose user id
       return Promise.resolve({
         ...session,
-        user: { ...session.user, id: token.sub },
+        user: {
+          ...session.user,
+          id: token.sub as string,
+          guildIds: (token as JWT).guildIds,
+        },
       });
+    },
+
+    async jwt(token, user) {
+      if (user?.guilds) {
+        token.guildIds = user.guilds.map((g: Guild) => g.id);
+      }
+
+      return token;
     },
   },
 });
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,35 @@
+import { Guild } from "../../pages/api/auth/[...nextauth]";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+      guildIds: string[];
+    };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image: string;
+    guilds: Guild[];
+  }
+
+  interface Profile {
+    id: string;
+    username: string;
+    email: string;
+    discriminator: string;
+    avatar: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    guildIds: string[];
+  }
+}
