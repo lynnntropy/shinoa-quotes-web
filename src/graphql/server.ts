38a2f5f5586a055b8c18e5264fcc7668ed398d8a
Veselin Romić <omegavesko@gmail.com>
2021-06-06 14:54:51 +0200

    feat: add session into GraphQL context

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -1,9 +1,23 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer, AuthenticationError } from "apollo-server-micro";
+import { getSession } from "next-auth/client";
 import resolvers from "./resolvers";
 // todo how do we do this properly?
 // @ts-ignore
 import typeDefs from "./schema.graphql";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const session = await getSession({ req });
+    if (session === null) {
+      throw new AuthenticationError(
+        "You must be logged in to access the GraphQL API."
+      );
+    }
+
+    return { session };
+  },
+});
 
 export default server;
