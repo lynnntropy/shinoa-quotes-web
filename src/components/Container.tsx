e571a28e5207c8186454ade83d7f2939b539151c
Veselin Romić <omegavesko@gmail.com>
2021-08-29 23:09:35 +0200

    fix: container title alignment

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,13 +6,16 @@ export interface ContainerProps {}
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
     <>
-      <Link href={homePage()}>
-        <a>
-          <h1 className="mt-14 text-center text-2xl font-bold text-gray-400">
-            Shinoa Quotes
-          </h1>
-        </a>
-      </Link>
+      <div className="mt-14 flex justify-center">
+        <Link href={homePage()}>
+          <a>
+            <h1 className="inline-block text-2xl font-bold text-gray-400">
+              Shinoa Quotes
+            </h1>
+          </a>
+        </Link>
+      </div>
+
       <main className="max-w-xl mx-auto my-8 px-8 py-7 bg-gray-700 rounded-lg shadow-md">
         {children}
       </main>
